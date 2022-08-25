f5c528663b2b1f5c701d8f7760709e167883e643
Satveer Brar <satveerbrar180@gmail.com>
2022-08-25 13:56:38 -0400

    Typo fixed in user model

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
         required: true,
     },
     discordId: String,
-    gihubProfile: String,
+    githubProfile: String,
     preferredMethodOfContact: String,
     isNewUser: Boolean,
     projectsCreated: [
