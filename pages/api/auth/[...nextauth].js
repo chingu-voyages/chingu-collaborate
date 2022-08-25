6a7fc5c1839e5b05c96ebc2e5593d229b8b015b2
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-08-25 10:49:59 -0400

    Complete integrating front end for authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
+
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
