a20caf96d7576a3fbae04c870251f55b146f3dea
Satveer Brar <satveerbrar180@gmail.com>
2022-08-24 22:28:57 -0400

    User model created

--- models/user.js
+++ models/user.js
@@ -1,12 +1,42 @@
 const {Schema, model, models} = require('mongoose');
 
 const userSchema = new Schema({
-    name: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     email: {
         type: String,
         required: true,
         unique: true,
-    }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    discordId: String,
+    gihubProfile: String,
+    preferredMethodOfContact: String,
+    isNewUser: Boolean,
+    projectsCreated: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project'
+        }   
+    ],
+    projectsRequested: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project'
+        }   
+    ],
+    projectsApplied: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Project'
+        }   
+    ],
 });
 
 const User = models.User || model('User', userSchema)
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connectToDatabase = async () => { 
+const connectToDatabase = async () => { 
     try {
         mongoose.connect(process.env.MONGODB_URI);
     }
