5021cc4de30e5fb7091ebebb70c8ee8e1f15e566
aresous <aresous@gmail.com>
2022-08-31 16:16:54 -0400

    add return and status code

--- pages/api/addUser.js
+++ pages/api/addUser.js
@@ -3,11 +3,12 @@ import User from '../../models/user'
 
 export default async function addUser(req, res) {
     try {
-        await connectToDatabase();
+        await connectToDatabase()
         console.log('Connected to MongoDB')
-        const user = await User.create(req.body);
-        res.json({user});   
+        const user = await User.create(req.body)
+        return res.json({ user })
     } catch (err) {
         console.log(err)
+        return res.status(400).json(err)
     }
 }
