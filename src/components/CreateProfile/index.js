9537fb341d197368122c18185271d5eebb0c7044
aresous <aresous@gmail.com>
2022-08-31 16:17:11 -0400

    add form submission

--- src/components/CreateProfile/index.js
+++ src/components/CreateProfile/index.js
@@ -9,8 +9,49 @@ import {
     HStack,
     Button,
 } from '@chakra-ui/react'
-
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 function CreateProfile() {
+    const router = useRouter()
+    const { data: session, status } = useSession()
+    const loading = status === 'loading'
+    const [username, setUsername] = useState('')
+    const [location, setLocation] = useState('')
+    const [email, setEmail] = useState('')
+    const [discordId, setDiscordId] = useState('')
+    const [methodOfContact, setMethodOfContact] = useState('email')
+    const [githubLink, setGithubLink] = useState('')
+    function formSubmit() {
+        let formData = {
+            username,
+            location,
+            email,
+            discordId,
+            preferredMethodOfContact: methodOfContact,
+            githubLink,
+            discordUsername: session.user.name,
+            userId: session.userId,
+        }
+
+        fetch(`/api/addUser`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data, ' api response')
+                if (!data.errors) {
+                    router.push('/projects')
+                }
+            })
+
+        // console.log('formSubmit function')
+        // console.log('formData', formData)
+    }
     const inputMarginBottom = '1rem'
     const labelMarginBottom = '0'
     return (
@@ -26,18 +67,41 @@ function CreateProfile() {
             <Heading size="xl">Create Profile</Heading>
             <FormControl>
                 <FormLabel marginBottom={labelMarginBottom}>Username</FormLabel>
-                <Input type="text" marginBottom={inputMarginBottom} />
+                <Input
+                    onChange={(e) => {
+                        setUsername(e.target.value)
+                    }}
+                    type="text"
+                    marginBottom={inputMarginBottom}
+                />
 
                 <FormLabel marginBottom={labelMarginBottom}>Location</FormLabel>
-                <Input type="text" marginBottom={inputMarginBottom} />
+                <Input
+                    onChange={(e) => {
+                        setLocation(e.target.value)
+                    }}
+                    type="text"
+                    marginBottom={inputMarginBottom}
+                />
 
                 <FormLabel marginBottom={labelMarginBottom}>Email</FormLabel>
-                <Input type="email" />
+                <Input
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}
+                    type="email"
+                />
 
                 <FormLabel marginBottom={labelMarginBottom}>
                     Discord ID
                 </FormLabel>
-                <Input type="text" marginBottom={inputMarginBottom} />
+                <Input
+                    onChange={(e) => {
+                        setDiscordId(e.target.value)
+                    }}
+                    type="text"
+                    marginBottom={inputMarginBottom}
+                />
 
                 <FormLabel marginBottom={labelMarginBottom}>
                     Preferred Method of Contact
@@ -47,16 +111,43 @@ function CreateProfile() {
                     marginBottom={inputMarginBottom}
                 >
                     <HStack spacing="24px">
-                        <Radio value="email">Email</Radio>
-                        <Radio value="discord">Discord</Radio>
+                        <Radio
+                            onChange={(e) => {
+                                setMethodOfContact(e.target.value)
+                            }}
+                            value="email"
+                        >
+                            Email
+                        </Radio>
+                        <Radio
+                            onChange={(e) => {
+                                setMethodOfContact(e.target.value)
+                            }}
+                            value="discord"
+                        >
+                            Discord
+                        </Radio>
                     </HStack>
                 </RadioGroup>
 
                 <FormLabel marginBottom={labelMarginBottom}>
                     Github Profile Link
                 </FormLabel>
-                <Input type="text" marginBottom={inputMarginBottom} />
-                <Button colorScheme="green" size="lg" width="100%">
+                <Input
+                    onChange={(e) => {
+                        setGithubLink(e.target.value)
+                    }}
+                    type="text"
+                    marginBottom={inputMarginBottom}
+                />
+                <Button
+                    onClick={() => {
+                        formSubmit()
+                    }}
+                    colorScheme="green"
+                    size="lg"
+                    width="100%"
+                >
                     Proceed
                 </Button>
             </FormControl>
