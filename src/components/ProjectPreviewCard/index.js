408aaee55efbf1f8c1f0465bac7cbae8357e6f04
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-09-06 12:34:30 -0400

    Add onClick behaviour for project cards, and configure routing for project specific pages

--- src/components/ProjectPreviewCard/index.js
+++ src/components/ProjectPreviewCard/index.js
@@ -10,6 +10,8 @@ import {
     HStack,
     Tag,
     TagLabel,
+    LinkBox,
+    LinkOverlay,
 } from '@chakra-ui/react'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { BiUser, BiTimeFive, BiHourglass } from 'react-icons/bi'
@@ -30,7 +32,6 @@ function ProjectPreviewCard({ project, isAdmin }) {
     const getAdmin = async () => {
         const response = await fetch(`/api/user/${project.admin}`)
         const user = await response.json()
-        console.log(user)
         setAdmin(user.username)
         setLocation(user.location)
     }
@@ -59,78 +60,87 @@ function ProjectPreviewCard({ project, isAdmin }) {
     }
 
     return (
-        <Flex
-            borderWidth="2px"
-            borderRadius="lg"
-            borderColor={isAdmin ? 'green.500' : ''}
-            width="90%"
-            padding="2rem"
-            flexDirection="column"
-            textAlign="left"
-            gap={2}
-        >
-            <Flex align="center" justify="space-between">
-                <Heading size="lg" noOfLines={1}>
-                    {project.title}
-                </Heading>
-                <Menu>
-                    <MenuButton
-                        variant="ghost"
-                        as={IconButton}
-                        icon={<BsThreeDotsVertical fontSize={20} />}
-                    />
-                    <MenuList>
-                        {isAdmin ? (
-                            <MenuItem
-                                onClick={() => deleteProjectIdea(project._id)}
-                            >
-                                Delete
-                            </MenuItem>
-                        ) : (
-                            <MenuItem>Report</MenuItem>
-                        )}
-                    </MenuList>
-                </Menu>
+        <LinkBox as="article" width="90%">
+            <Flex
+                borderWidth="2px"
+                borderRadius="lg"
+                borderColor={isAdmin ? 'green.500' : ''}
+                width="100%"
+                padding="2rem"
+                flexDirection="column"
+                textAlign="left"
+                gap={2}
+            >
+                <Flex align="center" justify="space-between">
+                    <LinkOverlay href={`/projects/${project._id}`}>
+                        <Heading size="lg" noOfLines={1}>
+                            {project.title}
+                        </Heading>
+                    </LinkOverlay>
+                    <Menu>
+                        <MenuButton
+                            variant="ghost"
+                            as={IconButton}
+                            icon={<BsThreeDotsVertical fontSize={20} />}
+                        />
+                        <MenuList>
+                            {isAdmin ? (
+                                <MenuItem
+                                    onClick={() =>
+                                        deleteProjectIdea(project._id)
+                                    }
+                                >
+                                    Delete
+                                </MenuItem>
+                            ) : (
+                                <MenuItem>Report</MenuItem>
+                            )}
+                        </MenuList>
+                    </Menu>
 
-                {/* <IconButton
+                    {/* <IconButton
                     variant="ghost"
                     icon={<BsThreeDotsVertical fontSize={20} />}
                 /> */}
-            </Flex>
-            <Flex gap={10}>
-                <Flex align="center" gap={1}>
-                    <BiUser />
-                    <Heading size="sm" fontWeight={500}>{`${admin} `}</Heading>
                 </Flex>
-                {location != '' && (
+                <Flex gap={10}>
                     <Flex align="center" gap={1}>
-                        <BiTimeFive />
-                        <Heading size="sm" fontWeight={500}>
-                            {location}
-                        </Heading>
+                        <BiUser />
+                        <Heading
+                            size="sm"
+                            fontWeight={500}
+                        >{`${admin} `}</Heading>
                     </Flex>
-                )}
-            </Flex>
-            <Flex align="center" gap={1}>
-                <BiHourglass />
-                <Heading
-                    size="sm"
-                    fontWeight={500}
-                    color="red.500"
-                >{`Expires in ${remaningDays}`}</Heading>
-            </Flex>
+                    {location != '' && (
+                        <Flex align="center" gap={1}>
+                            <BiTimeFive />
+                            <Heading size="sm" fontWeight={500}>
+                                {location}
+                            </Heading>
+                        </Flex>
+                    )}
+                </Flex>
+                <Flex align="center" gap={1}>
+                    <BiHourglass />
+                    <Heading
+                        size="sm"
+                        fontWeight={500}
+                        color="red.500"
+                    >{`Expires in ${remaningDays}`}</Heading>
+                </Flex>
 
-            <HStack spacing={2}>
-                {project.technologies.map((tech, index) => (
-                    <Tag key={index} variant="solid" colorScheme="gray">
-                        <TagLabel>{tech}</TagLabel>
-                    </Tag>
-                ))}
-            </HStack>
-            <Text fontSize="md" noOfLines={[4, 4, 3]}>
-                {project.details}
-            </Text>
-        </Flex>
+                <HStack spacing={2}>
+                    {project.technologies.map((tech, index) => (
+                        <Tag key={index} variant="solid" colorScheme="gray">
+                            <TagLabel>{tech}</TagLabel>
+                        </Tag>
+                    ))}
+                </HStack>
+                <Text fontSize="md" noOfLines={[4, 4, 3]}>
+                    {project.details}
+                </Text>
+            </Flex>
+        </LinkBox>
     )
 }
 
