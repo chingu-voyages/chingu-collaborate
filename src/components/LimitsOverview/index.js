e89b31b5b6f91ba657a8d9cb0df92014156ecebf
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-09-12 17:13:34 -0400

    Add tooltip beside title, change title to Project Limits, shorten description of limits, add conditional coloring to limits

--- src/components/LimitsOverview/index.js
+++ src/components/LimitsOverview/index.js
@@ -1,6 +1,10 @@
-import { Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text, Tooltip } from '@chakra-ui/react'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 
-function LimitsOverview({ projectsCreated = 1, projectsRequested = 0 }) {
+function LimitsOverview({ projectsCreated, projectsRequested }) {
+    const CREATELIMIT = 1
+    const REQUESTLIMIT = 5
+
     return (
         <Flex
             width="90%"
@@ -9,18 +13,59 @@ function LimitsOverview({ projectsCreated = 1, projectsRequested = 0 }) {
             textAlign="left"
             gap={4}
         >
-            <Heading size="lg">Weekly Limits</Heading>
+            <Flex align="center" gap={2}>
+                <Heading size="lg">Project Limits</Heading>
+                <Tooltip
+                    label="Note created projects are automatically expired in a week (7 days), and requested projects expire when the project itself expires, or if the project admin responds to your request."
+                    fontSize="xs"
+                    placement="right"
+                    hasArrow
+                >
+                    <span>
+                        <AiOutlineInfoCircle />
+                    </span>
+                </Tooltip>
+            </Flex>
             <Flex justify="space-evenly">
                 <Flex gap={2} align="center">
-                    <Heading>{`${projectsCreated}/1`}</Heading>
-                    <Text fontSize="sm" fontWeight={600}>
-                        Project Ideas Posted
+                    <Heading
+                        color={
+                            projectsCreated < CREATELIMIT
+                                ? 'gray.700'
+                                : 'red.700'
+                        }
+                    >{`${projectsCreated}/${CREATELIMIT}`}</Heading>
+                    <Text
+                        fontSize="sm"
+                        fontWeight={600}
+                        color={
+                            projectsCreated < CREATELIMIT
+                                ? 'gray.700'
+                                : 'red.700'
+                        }
+                    >
+                        Created
                     </Text>
                 </Flex>
+
                 <Flex gap={2} align="center">
-                    <Heading>{`${projectsRequested}/5`}</Heading>
-                    <Text fontSize="sm" fontWeight={600}>
-                        Projects Requested
+                    <Heading
+                        color={
+                            projectsRequested < REQUESTLIMIT
+                                ? 'gray.700'
+                                : 'red.700'
+                        }
+                    >{`${projectsRequested}/${REQUESTLIMIT}`}</Heading>
+                    <Text
+                        color={
+                            projectsRequested < REQUESTLIMIT
+                                ? 'gray.700'
+                                : 'red.700'
+                        }
+                        fontSize="sm"
+                        fontWeight={600}
+                    >
+                        Requested
                     </Text>
                 </Flex>
             </Flex>
