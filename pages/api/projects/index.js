5d591c1ffa75e053199fe9bb71edcd2d0f9c2142
Satveer Brar <satveerbrar180@gmail.com>
2022-09-13 01:11:36 -0400

    Separate function to validate POST project body

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -2,6 +2,30 @@ import connectToDatabase from '../../../utils/dbConnect'
 import Project from '../../../models/project'
 import { DateTime } from 'luxon'
 
+function validateBody(title, technologies, details, admin, timezone) {
+    if (title == undefined || title == '') {
+        return 'Title parameter is required'
+    } else if (technologies == undefined || technologies == '') {
+        return 'Technologies parameter is required'
+    } else if (details == undefined || details == '') {
+        return 'Details parameter is required'
+    } else if (admin == undefined || admin == '') {
+        return 'admin parameter is required'
+    } else if (timezone == undefined || timezone == '') {
+        return 'timezone parameter is required'
+    } else if (typeof title !== 'string') {
+        return 'Title parameter should be string'
+    } else if (title.length < 5 || title.length > 21) {
+        return 'Title parameter length should be between 5 to 20'
+    } else if (technologies.length < 1) {
+        return 'At least one technology should be selected'
+    } else if (details.length > 800) {
+        return 'Description should be less than 800 characters'
+    } else {
+        return ''
+    }
+}
+
 export default async function handler(req, res) {
     const { method } = req
     connectToDatabase()
@@ -20,46 +44,21 @@ export default async function handler(req, res) {
             break
         case 'POST':
             const { title, technologies, details, admin, timezone } = req.body
-            if (title == undefined) {
-                return res
-                    .status(400)
-                    .send({ error: 'Title parameter is required' })
-            }
-            if (technologies == undefined) {
-                return res
-                    .status(400)
-                    .send({ error: 'Technologies parameter is required' })
-            }
-            if (details == undefined) {
-                return res
-                    .status(400)
-                    .send({ error: 'Details parameter is required' })
-            }
-            if (typeof title !== 'string') {
-                return res
-                    .status(400)
-                    .send({ error: 'Title parameter should be string' })
-            }
-            if (title.length < 5 || title.length > 21) {
-                return res.status(400).send({
-                    error: 'Title parameter length should be between 5 to 20',
-                })
-            }
-            if (technologies.length < 1) {
-                return res.status(400).send({
-                    error: 'At least one technology should be selected',
-                })
-            }
-            if (details.length > 800) {
-                return res.status(400).send({
-                    error: 'Description should be less than 800 characters',
-                })
+            const validationResponse = validateBody(
+                title,
+                technologies,
+                details,
+                admin,
+                timezone
+            )
+
+            if (validationResponse != '') {
+                return res.status(400).send({ error: validationResponse })
             }
             try {
                 const project = new Project(req.body)
 
-                const now = DateTime.now()
-                //this logic is to get proper date format for project.datePosted
+                const now = DateTime.now() //this logic is to get proper date format for project.datePosted
                 const datePostedString = now.toLocaleString(
                     DateTime.DATETIME_MED
                 )
