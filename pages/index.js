cbbdde25b55377a97771cc6bff4307f495c1d59c
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-09-14 11:42:02 -0400

    Refactor Home page

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,26 @@ import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import Wrapper from '../src/components/Wrapper'
+import { Heading, Progress, Stack } from '@chakra-ui/react'
 
 export default function Home() {
     const { data: session, status } = useSession()
-    let routePushed = false
     const router = useRouter()
 
     useEffect(() => {
-        if (session?.dbUser && !routePushed) {
+        if (session?.dbUser) {
             router.push('/projects')
-            routePushed = true
         }
-    }, [session])
+    }, [session, router])
 
     return (
         <Wrapper session={session} status={status}>
-            Redirecting...
+            <Heading size="md" marginTop={5} marginBottom={2}>
+                Redirecting
+            </Heading>
+            <Stack width="90%">
+                <Progress size="xs" isIndeterminate colorScheme="green" />
+            </Stack>
         </Wrapper>
     )
 }
