915aa18692404c6c23571180a531e65bb0c77d59
Satveer Brar <satveerbrar180@gmail.com>
2022-09-15 21:37:52 -0400

    C:/Program Files/Git/api/projects routes protected

--- src/pages/api/projects/[id].js
+++ src/pages/api/projects/[id].js
@@ -1,76 +1,87 @@
 import connectToDatabase from '../../../utils/dbConnect'
 import Project from '../../../models/project'
+import { unstable_getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
 
 export default async function handler(req, res) {
-    const { method } = req
-    connectToDatabase()
+    const session = await unstable_getServerSession(req, res, authOptions)
+    if (session) {
+        const { method } = req
+        connectToDatabase()
 
-    switch (method) {
-        case 'GET':
-            try {
-                const project = await Project.findById(req.query.id)
-                    .populate('admin')
-                    .populate('currentMembers')
-                    .populate('requestedMembers')
-                return res.status(200).json(project)
-            } catch (err) {
-                return res.status(500).json({ message: 'Project Not found' })
-            }
-            break
-        case 'PATCH':
-            const { user_id, requestType } = req.body
-            const { id } = req.query
-            const options = {
-                new: true, // THis option is to return updated in same update request
-            }
-            try {
-                if (requestType == 'requestForProject') {
-                    const project = await Project.findByIdAndUpdate(
-                        id,
-                        { $push: { requestedMembers: user_id } },
-                        options
-                    )
+        switch (method) {
+            case 'GET':
+                try {
+                    const project = await Project.findById(req.query.id)
+                        .populate('admin')
+                        .populate('currentMembers')
+                        .populate('requestedMembers')
                     return res.status(200).json(project)
-                } else if (
-                    requestType == 'rejectProject' ||
-                    requestType == 'withdrawFromProject'
-                ) {
-                    const project = await Project.findByIdAndUpdate(
-                        id,
-                        { $pull: { requestedMembers: user_id } },
-                        options
-                    )
-                    return res.status(200).json(project)
-                } else if (requestType == 'approveProject') {
-                    await Project.findByIdAndUpdate(
-                        id,
-                        { $pull: { requestedMembers: user_id } },
-                        options
-                    )
-                    const project = await Project.findByIdAndUpdate(
-                        id,
-                        { $push: { currentMembers: user_id } },
-                        options
-                    )
-                    return res.status(200).json(project)
-                } else {
-                    return res.status(400).json('requestType is invalid')
+                } catch (err) {
+                    return res
+                        .status(500)
+                        .json({ message: 'Project Not found' })
                 }
-            } catch (err) {
-                return res.status(500).json(err)
-            }
-            break
-        case 'DELETE':
-            try {
-                const project = await Project.findByIdAndDelete(req.query.id)
-                return res.status(200).json(project)
-            } catch (err) {
-                return res.status(500).json(err)
-            }
-            break
-        default:
-            return res
-                .status(400)
-                .json({ message: 'Method type not supported' })
+                break
+            case 'PATCH':
+                const { user_id, requestType } = req.body
+                const { id } = req.query
+                const options = {
+                    new: true, // THis option is to return updated in same update request
+                }
+                try {
+                    if (requestType == 'requestForProject') {
+                        const project = await Project.findByIdAndUpdate(
+                            id,
+                            { $push: { requestedMembers: user_id } },
+                            options
+                        )
+                        return res.status(200).json(project)
+                    } else if (
+                        requestType == 'rejectProject' ||
+                        requestType == 'withdrawFromProject'
+                    ) {
+                        const project = await Project.findByIdAndUpdate(
+                            id,
+                            { $pull: { requestedMembers: user_id } },
+                            options
+                        )
+                        return res.status(200).json(project)
+                    } else if (requestType == 'approveProject') {
+                        await Project.findByIdAndUpdate(
+                            id,
+                            { $pull: { requestedMembers: user_id } },
+                            options
+                        )
+                        const project = await Project.findByIdAndUpdate(
+                            id,
+                            { $push: { currentMembers: user_id } },
+                            options
+                        )
+                        return res.status(200).json(project)
+                    } else {
+                        return res.status(400).json('requestType is invalid')
+                    }
+                } catch (err) {
+                    return res.status(500).json(err)
+                }
+                break
+            case 'DELETE':
+                try {
+                    const project = await Project.findByIdAndDelete(
+                        req.query.id
+                    )
+                    return res.status(200).json(project)
+                } catch (err) {
+                    return res.status(500).json(err)
+                }
+                break
+            default:
+                return res
+                    .status(400)
+                    .json({ message: 'Method type not supported' })
+        }
+    } else {
+        return res.status(401).send({ error: 'You need to sign in first' })
     }
 }
--- src/pages/api/projects/index.js
+++ src/pages/api/projects/index.js
@@ -2,51 +2,61 @@ import connectToDatabase from '../../../utils/dbConnect'
 import Project from '../../../models/project'
 import { DateTime } from 'luxon'
 import { validateProjectBody } from '../../../utils/validation'
+import { unstable_getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
 
 export default async function handler(req, res) {
-    const { method } = req
-    connectToDatabase()
+    const session = await unstable_getServerSession(req, res, authOptions)
+    if (session) {
+        const { method } = req
+        connectToDatabase()
 
-    switch (method) {
-        case 'GET':
-            try {
-                const projects = await Project.find()
-                    .populate('admin')
-                    .populate('currentMembers')
-                    .populate('requestedMembers')
-                return res.status(200).json(projects)
-            } catch (err) {
-                return res.status(500).json({ message: 'Projects Not found' })
-            }
-            break
-        case 'POST':
-            const { title, technologies, details, admin, timezone } = req.body
-            const validationResponse = validateProjectBody(
-                title,
-                technologies,
-                details,
-                admin,
-                timezone
-            )
+        switch (method) {
+            case 'GET':
+                try {
+                    const projects = await Project.find()
+                        .populate('admin')
+                        .populate('currentMembers')
+                        .populate('requestedMembers')
+                    return res.status(200).json(projects)
+                } catch (err) {
+                    return res
+                        .status(500)
+                        .json({ message: 'Projects Not found' })
+                }
+                break
+            case 'POST':
+                const { title, technologies, details, admin, timezone } =
+                    req.body
+                const validationResponse = validateProjectBody(
+                    title,
+                    technologies,
+                    details,
+                    admin,
+                    timezone
+                )
 
-            if (validationResponse != true) {
-                return res.status(400).send({ error: validationResponse })
-            }
-            try {
-                const project = new Project(req.body)
-                const now = DateTime.now()
-                project.createdAt = now
-                project.expiresIn = now.plus({ week: 1 })
-                project.admin = admin
-                await project.save()
-                return res.status(200).json(project)
-            } catch (err) {
-                return res.status(500).json(err)
-            }
-            break
-        default:
-            return res
-                .status(400)
-                .json({ message: 'Method type not supported' })
+                if (validationResponse != true) {
+                    return res.status(400).send({ error: validationResponse })
+                }
+                try {
+                    const project = new Project(req.body)
+                    const now = DateTime.now()
+                    project.createdAt = now
+                    project.expiresIn = now.plus({ week: 1 })
+                    project.admin = admin
+                    await project.save()
+                    return res.status(200).json(project)
+                } catch (err) {
+                    return res.status(500).json(err)
+                }
+                break
+            default:
+                return res
+                    .status(400)
+                    .json({ message: 'Method type not supported' })
+        }
+    } else {
+        return res.status(401).send({ error: 'You need to sign in first' })
     }
 }
